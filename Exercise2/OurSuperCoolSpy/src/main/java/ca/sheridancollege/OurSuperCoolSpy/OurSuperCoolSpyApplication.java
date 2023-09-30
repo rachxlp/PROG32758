b4b36cd09cba9d142ec052678ede83ab63b5c732
Rachel <prasadra@sheridancollege.ca>
2023-09-29 21:51:32 -0400

    final update v2

---
+++ Exercise2/OurSuperCoolSpy/src/main/java/ca/sheridancollege/OurSuperCoolSpy/OurSuperCoolSpyApplication.java
@@ -0,0 +1,20 @@
+/*
+ * Name:  Rachel Prasad
+ * Student ID: 991702269
+ * Assignment:  Exercise 2 - OurSuperCoolSpyApplication
+ * Program: Computer Systems and Network Engineering (Co-op)
+ */
+
+package ca.sheridancollege.OurSuperCoolSpy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurSuperCoolSpyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurSuperCoolSpyApplication.class, args);
+	}
+
+}
---
+++ Exercise2/OurSuperCoolSpy/src/test/java/ca/sheridancollege/OurSuperCoolSpy/OurSuperCoolSpyApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.OurSuperCoolSpy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurSuperCoolSpyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
